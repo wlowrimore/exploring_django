decccc69556f98e9c6beae22e2f8d5e699c9cde4
William Lowrimore <wlowrimore@gmail.com>
2023-06-22 17:57:38 -0500

    initial setup with one basic home route

---
+++ hello/views.py
@@ -0,0 +1,21 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+
+def home(request):
+  return HttpResponse("Hello, Django!")
+
+
+def hello_there(request, name):
+  now = datetime.now()
+  formatted_now = now.strftime("%A, %d %8, %Y at %X")
+  
+  match_object = re.match("[a-zA-Z]+", name)
+  
+  if match_object:
+    clean_name = match_object.group(0)
+  else:
+    clean_name = "Friend"
+    
+  content = "Hello there, " + clean_name + "! It's " + formatted_now
+  return HttpResponse(content)
