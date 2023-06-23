f9cbdfed103e7d7d3c4a508ed36d52eea01df1f2
William Lowrimore <wlowrimore@gmail.com>
2023-06-22 21:12:48 -0500

    adds logged messages to home page

--- hello/models.py
+++ hello/models.py
@@ -8,6 +8,6 @@ class LogMessage(models.Model):
   
   def __str__(self):
     """Returns a string representation of a message."""
-    dat = timezone.localtime(self.log_date)
+    date = timezone.localtime(self.log_date)
     return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
 
--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,18 @@
 import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
   
 
 def about(request):
@@ -26,3 +34,15 @@ def hello_there(request, name):
         }
     )
     
+    
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})   
